01d99484955c732d310ffa68214720fedeaba944
mjtoolbox <michael.jo@gmail.com>
2013-07-06 18:45:50 +0000

    Bug fixed

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(readFile("index.html"));
 });
 
 
